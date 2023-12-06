10a83f0773a8d441bbe16bfc6fd0b755c2bc0ec3
Gabriel Gonzalez <gabriel.gonzalez.unt@gmail.com>
2023-12-06 15:48:05 -0300

    Finalizada parte 1, botón Salir y botón Usuarios funcionando

--- Practica23-G30/src/principal/controladores/ControladorPrincipal.java
+++ Practica23-G30/src/principal/controladores/ControladorPrincipal.java
@@ -5,13 +5,16 @@
  */
 package principal.controladores;
 
-
+import interfaces.IControladorPrincipal;
 import interfaces.IGestorProductos;
 import interfaces.IGestorUsuarios;
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import productos.modelos.Producto;
 import java.util.List;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
+import principal.vistas.VentanaPrincipal;
 import productos.modelos.Categoria;
 import productos.modelos.Estado;
 import productos.modelos.GestorProductos;
@@ -23,15 +26,25 @@ import usuarios.vistas.VentanaAMUsuario;
  *
  * @author root
  */
-public class ControladorPrincipalTP7  {
+public class ControladorPrincipal implements IControladorPrincipal {
+
+    private VentanaPrincipal ventana;
+
+    public ControladorPrincipal() {
+        this.ventana = new VentanaPrincipal(this);
+        this.ventana.setTitle(TITULO);
+        this.ventana.setLocationRelativeTo(null);
+        this.ventana.setVisible(true);
+    }
+
     public static void main(String[] args) {
         IGestorProductos gp = GestorProductos.instanciar();
         IGestorUsuarios gu = GestorUsuarios.instanciar();
-        List<Usuario> usuarios =new ArrayList<>();
+        List<Usuario> usuarios = new ArrayList<>();
         //Creación de productos
 
         /*Usar una vez la creación para verificar que se guardan en el archivo*/
-        System.out.println(gp.crearProducto(1, "Producto1", 1.0f, Categoria.ENTRADA, Estado.DISPONIBLE));       
+        System.out.println(gp.crearProducto(1, "Producto1", 1.0f, Categoria.ENTRADA, Estado.DISPONIBLE));
         System.out.println(gp.crearProducto(2, "Producto2", 2.0f, Categoria.PLATO_PRINCIPAL, Estado.DISPONIBLE));
         System.out.println(gp.crearProducto(3, "Producto3", 3.0f, Categoria.POSTRE, Estado.DISPONIBLE));
         System.out.println(gp.crearProducto(3, "Producto4", 4.0f, Categoria.POSTRE, Estado.DISPONIBLE));
@@ -48,10 +61,10 @@ public class ControladorPrincipalTP7  {
         //sin categoría
         System.out.println(gp.crearProducto(4, "Producto4", 4.0f, Categoria.POSTRE, null));
         //sin estado
-    // Trabajo con productos
+        // Trabajo con productos
         System.out.println("Productos");
         System.out.println("=========");
-        for(Producto p : gp.menu()) {
+        for (Producto p : gp.menu()) {
             if (p.verPrecio() > 0) { //para no mostrar los 3 productos con precio negativo
                 p.mostrar();
                 System.out.println();
@@ -61,76 +74,109 @@ public class ControladorPrincipalTP7  {
 
         Producto unProducto1 = gp.obtenerProducto(1);
         Producto unProducto2 = gp.obtenerProducto(2);
-        
+
         System.out.println(gp.existeEsteProducto(unProducto1));
         Producto unProducto10 = new Producto(10, "Producto10", Categoria.ENTRADA, Estado.DISPONIBLE, 10.0f);
         //producto inexistente
         System.out.println(gp.existeEsteProducto(unProducto10));
-               
+
         List<Producto> productosBuscados = gp.verProductosPorCategoria(Categoria.POSTRE);
         System.out.println("Productos filtrados");
         System.out.println("===================");
-        for(Producto p : productosBuscados) {
+        for (Producto p : productosBuscados) {
             if (p.verPrecio() > 0) { //para no mostrar los 3 productos con precio negativo
                 p.mostrar();
                 System.out.println();
             }
         }
         System.out.println();
-        
+
         productosBuscados = gp.buscarProductos("Producto");
         System.out.println("Productos buscados");
         System.out.println("==================");
-        for(Producto p : productosBuscados) {
+        for (Producto p : productosBuscados) {
             if (p.verPrecio() > 0) { //para no mostrar los 3 productos con precio negativo
                 p.mostrar();
                 System.out.println();
             }
         }
         System.out.println();
-      
+
         System.out.println(gp.modificarProducto(unProducto1, 100, "Producto11", 10.0f, Categoria.PLATO_PRINCIPAL, Estado.NO_DISPONIBLE));
         //se le cambia la descripción, precio, categoría y estado
         //el código, por más que se lo pasa, no se modifica
         productosBuscados = gp.buscarProductos("Producto11");
         System.out.println("Productos buscados");
         System.out.println("==================");
-        for(Producto p : productosBuscados) {
+        for (Producto p : productosBuscados) {
             if (p.verPrecio() > 0) { //para no mostrar los 3 productos con precio negativo
                 p.mostrar();
                 System.out.println();
             }
         }
         System.out.println();
-        System.out.println(gp.modificarProducto(unProducto2, 1, "Producto2Modif", 10.0f, Categoria.PLATO_PRINCIPAL, Estado.DISPONIBLE));        
+        System.out.println(gp.modificarProducto(unProducto2, 1, "Producto2Modif", 10.0f, Categoria.PLATO_PRINCIPAL, Estado.DISPONIBLE));
         //sí se puede
-        
+
         System.out.println(gp.borrarProducto(unProducto1));
         //sí se puede
-        
+
         System.out.println("Productos");
         System.out.println("=========");
-        for(Producto p : gp.menu()) {
+        for (Producto p : gp.menu()) {
             if (p.verPrecio() > 0) { //para no mostrar los 3 productos con precio negativo
                 p.mostrar();
                 System.out.println();
             }
         }
         System.out.println();
-        
-        establecerLookAndFeel("Nimbus"); 
-        VentanaAMUsuario ventanaAUsuario = new VentanaAMUsuario(null);
-              
+
+//        establecerLookAndFeel("Nimbus");
+//        VentanaAMUsuario ventanaAUsuario = new VentanaAMUsuario(null);
+        IControladorPrincipal cp = new ControladorPrincipal();
+
         System.out.println("USUARIOS");
         System.out.println("=====");
-        usuarios=gu.verUsuarios();
-        for(Usuario u : usuarios) {
+        usuarios = gu.verUsuarios();
+        for (Usuario u : usuarios) {
             u.mostrar();
             System.out.println();
         }
 
-    } 
-    
+    }
+
+    @Override
+    public void btnUsuariosClic(ActionEvent evt) {
+        VentanaAMUsuario ventanaAUsuario = new VentanaAMUsuario(null);
+    }
+
+    @Override
+    public void btnProductosClic(ActionEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void btnPedidosClic(ActionEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void btnSalirClic(ActionEvent evt) {
+        int eleccion = JOptionPane.showOptionDialog(
+                this.ventana,
+                "¿Desea finalizar la sesión?",
+                TITULO, JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                new Object[]{"Si", "No"},
+                this
+        );
+        
+        if(eleccion == JOptionPane.YES_OPTION){
+            System.exit(0);
+        }
+    }
+
     public static void establecerLookAndFeel(String laf) {
         try {
             for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
@@ -141,8 +187,7 @@ public class ControladorPrincipalTP7  {
         } catch (Exception e) {
             try {
                 UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            } 
-            catch (Exception e2) {
+            } catch (Exception e2) {
             }
         }
     }
