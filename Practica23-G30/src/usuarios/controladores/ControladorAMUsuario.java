5d6eed5f42ae6f18fcba351bf231afbfab075cbd
Gabriel Gonzalez <gabriel.gonzalez.unt@gmail.com>
2023-12-07 22:19:22 -0300

    Añadidos métodos a ControladorAMUsuario

--- Practica23-G30/src/usuarios/controladores/ControladorAMUsuario.java
+++ Practica23-G30/src/usuarios/controladores/ControladorAMUsuario.java
@@ -4,10 +4,94 @@
  */
 package usuarios.controladores;
 
+import interfaces.IControladorAMUsuario;
+import interfaces.IGestorUsuarios;
+import static interfaces.IGestorUsuarios.EXITO_CREADO;
+import static interfaces.IGestorUsuarios.EXITO_MODIFICADO;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import javax.swing.JOptionPane;
+import usuarios.modelos.GestorUsuarios;
+import usuarios.modelos.ModeloComboPerfiles;
+import usuarios.modelos.Perfil;
+import usuarios.vistas.VentanaAMUsuario;
+import usuarios.vistas.VentanaUsuarios;
+
 /**
  *
  * @author gabri
  */
-public class ControladorAMUsuario {
-    
+public class ControladorAMUsuario implements IControladorAMUsuario {
+
+    private VentanaAMUsuario ventana;
+    private IGestorUsuarios gu = GestorUsuarios.instanciar();
+
+    public ControladorAMUsuario(VentanaUsuarios ventanaPadre) {
+        this.ventana = new VentanaAMUsuario(this, ventanaPadre, true);
+        this.ventana.setTitle(TITULO_NUEVO);
+        this.ventana.verTxtCorreo().requestFocus();
+        this.ventana.verPerfil().setModel(new ModeloComboPerfiles());
+        this.ventana.setLocationRelativeTo(null);
+        this.ventana.setVisible(true);
+    }
+
+    public void guardar() {
+        String correo = this.ventana.verTxtCorreo().getText().trim();
+        String apellido = this.ventana.verTxtApellido().getText().trim();
+        String nombre = this.ventana.verTxtNombre().getText().trim();
+        String clave = new String(this.ventana.verPassClave().getPassword());
+        String claveRepetida = new String(this.ventana.verPassClaveRepetida().getPassword());
+        Perfil perfil = ((ModeloComboPerfiles) this.ventana.verPerfil().getModel()).obtenerPerfil();
+
+        String resultado;
+        if (this.ventana.verTxtCorreo().isEnabled()) {
+            resultado = gu.crearUsuario(correo, apellido, nombre, perfil, clave, claveRepetida);
+        } else {
+            resultado = gu.modificarUsuario(gu.obtenerUsuario(correo), correo, apellido, nombre, perfil, clave, claveRepetida);
+        }
+
+        if (resultado == EXITO_CREADO || resultado == EXITO_MODIFICADO) {
+            JOptionPane.showMessageDialog(null, resultado, "Usuario Guardado", JOptionPane.INFORMATION_MESSAGE);
+            this.ventana.setVisible(false);
+            this.ventana.dispose();
+        } else {
+            JOptionPane.showMessageDialog(this.ventana, resultado, "ERROR", JOptionPane.WARNING_MESSAGE);
+        }
+    }
+
+    @Override
+    public void btnGuardarClic(ActionEvent evt) {
+        this.guardar();
+    }
+
+    @Override
+    public void btnCancelarClic(ActionEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void txtApellidoPresionarTecla(KeyEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void txtNombrePresionarTecla(KeyEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void txtCorreoPresionarTecla(KeyEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void passClavePresionarTecla(KeyEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void passClaveRepetidaPresionarTecla(KeyEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }
